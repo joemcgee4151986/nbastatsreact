ced0f3ac1a521a299dd63dd89595ad8a00afca6e
joemcgee4151986 <joerobertmcgee@gmail.com>
2023-09-26 20:01:19 +0900

    adding project to github

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function Header() {
+  return(
+  <button>
+    I'm a button
+  </button>
+  );
+}
+
+export default function myApp() {
+return(
+  <div>
+    <h1>
+      Hi. Bye
+    </h1>
+    <Header />
+  </div>
+)
+}
+
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'
+
+function Header() {
+  return(
+ <div className="Header">
+    <div className="Title">
+     <h1>NBA.com/Stats</h1><img src = "../src/assets/Headerimage.JPG" />
+ </div>
+ </div>
+  );
+}
+
+export default Header
+
