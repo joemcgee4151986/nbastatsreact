3ca518d56cb0ed0df3ea3342e5fb615492081aa8
joemcgee4151986 <joerobertmcgee@gmail.com>
2023-10-10 18:21:48 +0900

    added mobile friendly options for the boxes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,116 +1,80 @@
 import React from 'react';
 import styled from 'styled-components'
 import myHeaderImg from '../../assets/Headerimage.JPG'
-import { device } from '../Devices/Devices.jsx'
 
 
-const H1 = styled.h1`
-  margin-top: 0%;
-  font-size: 9vw;
-  font-style:Helvetica Condensed;
-  color: gray;
-  @media ${device.mobileM} {
-    font-size:4vw;
-    margin-top: -1%;
-   }
+const device = {
+  mobileS: "320px",
+  mobileM: "480px",
+  mobileL: "640px",
+  tablet: "768px",
+  laptop: "1024px",
+  desktop: "1280px",
+};
+
+
+const H1 = styled.div`
+  display: flex;
+  flex-shrink: 0;
+  position: static;
+  font-size: 6em;
+  padding: 20px;
+
+  @media ${device.mobileL} {
+    padding-top: 10px;
+  }
 `;
 
- 
-  const Box0 = styled.div`
-  display: flex;
-  flex-direction: column; 
-  
-    max-width: 100%;
-    height: 5vh;
-    padding: 20px;
-    margin-bottom: 2em;
-    background-color: #0039a6;
-    font-size: 3vh;
-    color: white;
-    font: Helvetica;
- background-color: white;
-
- @media ${device.mobileS} {
-  max-width: 100%;
-  white-space: nowrap;
- }
-
- 
-  `;
-
 const Box1 = styled.div`
-table-layout: fixed;
-width: 100%;
-font-size: 2em;
-white-space: nowrap;
-  background-color: #0039a6;
-  font-size: 2.5vh;
+  background-color: blue;
   color: white;
-  font: Helvetica;
-  @media ${device.mobileL} {
-    max-width: 100%;
-    white-space: nowrap;
-  
-   }
-`
+  padding-right: 0;
+  padding-left: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  font-size: 200%;
+`;
+
 const Box2 = styled.div`
-display: inline-flex;
-position: relative;
-min-width: 25%;
-font-size: 1.5em;
-text-align:center;
-color: white;
-background-color:GREY;
-outline: solid 1px white;
-font: Helvetica;
-@media ${device.mobileS} {
-  min-width: 25%;
-  white-space: nowrap;
-  font-size: 12px;
- }
- }
-
-`
-const Box3 = styled.div`
-display: inline-flex;
-position: relative;
-min-width: 25%;
-font-size: 1.5em;
-text-align:center;
-color: white;
-background-color:GREY;
-outline: solid 1px white;
-font: Helvetica;
-@media (max-width: 480px) {
-  table-layout: fixed;
-  width: 25%;
-  font-size: 1em;
-  white-space: nowrap;
+  display: inline-flex;
   flex-direction: row;
- }
-`
+  width: 25%;
+  background-color: gray;
+  color: white;
+  outline: solid 1px white;
 
-
+  @media ${device.mobileL} {
+    max-width: 25%;
+    white-space: nowrap;
+    font-size: 12px;
+    margin-left: auto;
+    margin-right: 0;
+  }
+`;
 
 const Image = styled.img`
-display: flex;
-flex-direction: row-reverse;
-position: relative;
-margin-left: auto;
-margin-right: -18px;
-margin-top: -145px;
-padding: 20px;
-width: 30%;
-@media ${device.mobileM} {
-  max-width: 375px;
-  white-space: nowrap;
- }
-`
+  display: flex;
+  flex-direction: row-reverse;
+  position: relative;
+  margin-left: auto;
+  margin-right: -18px;
+  margin-top: -155px;
+  padding: 20px;
+  width: 30%;
+
+  @media ${device.mobileS} {
+    max-width: 375px;
+    white-space: nowrap;
+  }
+`;
 
 function Header() {
   return(
     <div>
-     <Box0><H1>NBA.com/Stats</H1></Box0>
+      
+     <H1>NBA.com/Stats</H1>
+   
      <Image src={myHeaderImg} />
      <Box1>
       League Leaders
