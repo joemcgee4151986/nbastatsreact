f8cb5becd8d386b989f1c5d0e6129debc12c42bd
joemcgee4151986 <joerobertmcgee@gmail.com>
2023-10-12 18:50:21 +0900

    added mobile friendly image option so it does not pop up in a different part of the screen

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled, { css } from 'styled-components'
 import myHeaderImg from '../../assets/Headerimage.JPG'
-import { device } from '../Devices/Devices.jsx'
+
 
 const HTML = styled.html`
   margin: 0;
@@ -17,21 +17,24 @@ const BODY = styled.body`
 
 const mobileMediaQuery = css`
 @media (max-width: 768px) {
-  
- 
+    font-size: 30px;
+    margin-bottom: 67px;
+    margin-top: -19px;
+  }
   
 }
 `
 
 const H1 = styled.div`
 display: flex;
+width: 50;
   flex-shrink: 0;
-  position: relative; 
+   
   font-size: 6em;
   padding: 20px;
-  margin-top: 20px;
+  margin-top: -19px;
   color:black;
-  margin-bottom: 42px;
+  margin-bottom: 67px;
   ${mobileMediaQuery};
 `;
 
@@ -46,18 +49,15 @@ const mobileMediaBox0 = css`
   font-size: 16px;
 `
 const Box0 = styled.div`
-
-background-color:yellow;
+display: flex;
+flex-direction: row;
 word-wrap: break-word;
 padding-right: 0;
 padding-left: 0;
-display: flex;
-flex-direction: column;
-align-items: left;
-font-size: 200%;
-max-width:60%;
+width:70%;
   ${mobileMediaBox0};
 `;
+
 
 const Box1 = styled.div`
   background-color: blue;
@@ -66,21 +66,27 @@ const Box1 = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start; 
-  font-size: 2em;
+  font-size: 2.2em;
   margin-bottom: -5px;
 ${mobileMediaBox1};
  
 `;
-const HeaderWrapper = styled.div`
-  margin-bottom: 50px; 
-`;
+
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: left; 
 `;
 
-
+const mobileMediaBox2 = css`
+@media (max-width: 768px) {
+max-width: 25%;
+white-space: nowrap;
+font-size: 12px;
+margin-left: auto;
+margin-right: 0;
+}
+`
 const Box2 = styled.div`
   display: inline-flex;
   flex-direction: row;
@@ -88,31 +94,23 @@ const Box2 = styled.div`
   background-color: gray;
   color: white;
   outline: solid 1px white;
-
-  @media ${device.mobileL} {
-    max-width: 25%;
-    white-space: nowrap;
-    font-size: 12px;
-    margin-left: auto;
-    margin-right: 0;
-  }
+  ${mobileMediaBox2};
 `;
+const mobileImage = css`
+@media (max-width: 768px){
+height: 60px;
+}
+`
+const Image= styled.img `
+margin-left: 47.5%;
+  display: block;
+  height: 120px;
+background-repeat: no-repeat;
+background-size: contain;
+${mobileImage};
+`
 
-const Image = styled.img`
-  display: flex;
-  flex-direction: row-reverse;
-  position: absolute;
-  margin-left: auto;
-  margin-right: -18px;
-  margin-top: -155px;
-  padding: 20px;
-  width: 30%;
 
-  @media ${device.mobileS} {
-    max-width: 375px;
-    white-space: nowrap;
-  }
-`;
 
 function Header() {
   return(
@@ -122,8 +120,12 @@ function Header() {
       <Container>
       <Box0>
      <H1>NBA.com/Stats</H1>
+     
+     <Image src = {myHeaderImg}  />
+     
      </Box0>
-     {/*<Image src={myHeaderImg} />*/}
+    
+    
      <Box1>
       League Leaders
      </Box1>
